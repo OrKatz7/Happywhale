08ba4beb84a2e436b740efb77afab963ba3c5e4e
OrKatz7 <Or.Katz1@gmail.com>
2022-02-21 09:35:37 +0200

    Update kfold_split.py

--- utils/kfold_split.py
+++ utils/kfold_split.py
@@ -3,20 +3,8 @@ import pandas as pd
 import numpy as np
 from torch_utils import seed_torch
 
-n_fold=5
-input_csv = '../whale/train.csv'
-output_csv = "../whale/folds.csv"
-
-if __name__ == "__main__":
-    seed_torch(0)
+def split_kfold(input_csv,output_csv,n_fold):
     train = pd.read_csv(input_csv)
-#     classes = list(set(train.individual_id))
-#     classes_species = list(set(train.species))
-#     sorted(classes)
-#     sorted(classes_species)
-#     print(train.head())
-#     print(len(classes))
-#     print(len(classes_species))
     train['classes'] = train.groupby('individual_id').ngroup()
     train['classes_species'] = train.groupby('species').ngroup()
     df_list = []
@@ -32,3 +20,18 @@ if __name__ == "__main__":
     for row in range(n_fold):
         print(len(list(set(train[train.fold==row].classes_species))))
     train.to_csv(output_csv,index=False)
+
+
+if __name__ == "__main__":
+    seed_torch(0)
+    n_fold=5
+    input_csv = '../whale/train.csv'
+    output_csv = "../whale/folds.csv"
+    split_kfold(input_csv,output_csv,n_fold)
+#     classes = list(set(train.individual_id))
+#     classes_species = list(set(train.species))
+#     sorted(classes)
+#     sorted(classes_species)
+#     print(train.head())
+#     print(len(classes))
+#     print(len(classes_species))
