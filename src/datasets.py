bb5f679b183dfce259632b3f89686c537826f2f9
OrKatz7 <Or.Katz1@gmail.com>
2022-02-23 19:11:27 +0200

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -88,17 +88,18 @@ class TrainDataset(Dataset):
     
 
 class TestDataset(Dataset):
-    def __init__(self, df, transform=None):
+    def __init__(self, df,TEST_PATH, transform=None):
         self.df = df
         self.file_names = df['image'].values
         self.transform = transform
+        self.TEST_PATH = TEST_PATH
         
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         file_name = self.file_names[idx]
-        file_path = f'{TEST_PATH}/{file_name}'
+        file_path = f'{self.TEST_PATH}/{file_name}'
         image = cv2.imread(file_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         if self.transform:
