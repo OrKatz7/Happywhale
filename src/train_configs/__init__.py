e31638a8d1cbf3c9bd08512b7d4465ae3d00a504
OrKatz7 <Or.Katz1@gmail.com>
2022-03-03 16:30:27 +0200

    Update __init__.py

--- src/train_configs/__init__.py
+++ src/train_configs/__init__.py
@@ -1,4 +1,17 @@
+import train_configs.config1
+import train_configs.config2
+import train_configs.config3
+import train_configs.config4
+import train_configs.config5
+import train_configs.config6
 import train_configs.config7
 import train_configs.config8
 import train_configs.config9
 import train_configs.config10
+import train_configs.config11
+import train_configs.config12
+import train_configs.config13
+import train_configs.config14
+import train_configs.config15
+## work only from here
+import train_configs.config16
